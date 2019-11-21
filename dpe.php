9c6dd3453c501bd17df344e013200937d1b7c089
ilhooq <contact@sphilip.com>
2019-11-21 12:18:41 +0100

    Save form fields only in web context

--- dpe.php
+++ dpe.php
@@ -1,273 +1,292 @@
 <?php
 /**
- * This file is part of Joomla Estate Agency - Joomla! extension for real estate agency
+ * @package     Joomla.Plugin
+ * @subpackage  Jea.Dpe
  *
- * @package		Jea
- * @copyright	Copyright (C) 2011 PHILIP Sylvain. All rights reserved.
- * @license		GNU/GPL, see LICENSE.txt
- * Joomla Estate Agency is free software. This version may have been modified pursuant to the
- * GNU General Public License, and as distributed it includes or is derivative
- * of works licensed under the GNU General Public License or other free or open
- * source software licenses.
- *
+ * @copyright   Copyright (C) 2007 - 2019 PHILIP Sylvain. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
+defined('_JEXEC') or die;
 
-defined( '_JEXEC' ) or die( 'Restricted access' );
-
-
-jimport('joomla.event.plugin');
-
-class plgJeaDpe extends JPlugin
+class PlgJeaDpe extends JPlugin
 {
-    /**
-     * Constructor
-     *
-     * @param       object  $subject The object to observe
-     * @param       array   $config  An array that holds the plugin configuration
-     */
-    public function __construct(& $subject, $config)
-    {
-        parent::__construct($subject, $config);
-        $this->loadLanguage();
-    }
+	/**
+	 * @var string
+	 */
+	protected $autoloadLanguage = true;
 
-    /**
-     * onBeforeSaveProperty method
-     *
-     * @param string $namespace
-     * @param TableProperties $row
-     * @param boolean $is_new
-     * @return boolean
-     */
-    public function onBeforeSaveProperty($namespace, $row, $is_new)
-    {
-        $input = JFactory::getApplication()->input;
-        if ($input->get('dpe_energy') || $input->get('dpe_ges')) {
-            $row->dpe_energy = $input->getFloat('dpe_energy', -1.0);
-            $row->dpe_ges    = $input->getFloat( 'dpe_ges', -1.0);
-        }
+	/**
+	 * onBeforeSaveProperty method
+	 *
+	 * @param  string         $namespace  The form namespace
+	 * @param  TableProperty  $row        The property Db row instance
+	 * @param  boolean        $is_new     True if the property is new
+	 *
+	 * @return boolean  True on success
+	 */
+	public function onBeforeSaveProperty($namespace, $row, $is_new)
+	{
+		$app = JFactory::getApplication();
 
-        return true;
-    }
+		if ($app instanceof \Joomla\CMS\Application\WebApplication)
+		{
+			$input = $app->input;
 
-    /**
-     * onBeforeEndPane method (Called in the admin property form)
-     *
-     * @param TableProperties $row
-     * @return void
-     */
-    public function onBeforeEndPanels(&$row)
-    {
-        if ($row->dpe_energy === null) {
-            $row->dpe_energy = '-1';
-        }
+			if ($input->get('dpe_energy') || $input->get('dpe_ges'))
+			{
+				$row->dpe_energy = $input->getFloat('dpe_energy', - 1.0);
+				$row->dpe_ges = $input->getFloat('dpe_ges', - 1.0);
+			}
+		}
 
-        if ($row->dpe_ges === null) {
-            $row->dpe_ges = '-1';
-        }
+		return true;
+	}
 
-        $energyLabel = JText::_('PLG_JEA_DPE_ENERGY_CONSUMPTION');
-        $energyDesc  = $energyLabel .'::'. JText::_('PLG_JEA_DPE_ENERGY_CONSUMPTION_DESC');
-        $gesLabel = JText::_('PLG_JEA_DPE_EMISSIONS_GES');
-        $gesDesc  = $gesLabel .'::'. JText::_('PLG_JEA_DPE_EMISSIONS_GES_DESC');
+	/**
+	 * onBeforeEndPane method
+	 * Called in the admin property form
+	 *
+	 * @param  TableProperty  $row  The property Db row instance
+	 *
+	 * @return void
+	 */
+	public function onBeforeEndPanels(&$row)
+	{
+		if ($row->dpe_energy === null)
+		{
+			$row->dpe_energy = '-1';
+		}
 
-        $html ='
+		if ($row->dpe_ges === null)
+		{
+			$row->dpe_ges = '-1';
+		}
+
+		$energyLabel = JText::_('PLG_JEA_DPE_ENERGY_CONSUMPTION');
+		$energyDesc = $energyLabel . '::' . JText::_('PLG_JEA_DPE_ENERGY_CONSUMPTION_DESC');
+		$gesLabel = JText::_('PLG_JEA_DPE_EMISSIONS_GES');
+		$gesDesc = $gesLabel . '::' . JText::_('PLG_JEA_DPE_EMISSIONS_GES_DESC');
+
+		echo JHtml::_('sliders.panel', JText::_('PLG_JEA_DPE'), 'dpe-pane');
+
+		echo '
         <fieldset class="panelform">
           <ul class="adminformlist">
             <li>
-              <label for="dpe_energy" class="hasTip" title="' . $energyDesc.'">' . $energyLabel .' : </label>
-              <input type="text" name="dpe_energy" id="dpe_energy" value="'.$row->dpe_energy.'" class="numberbox" size="5" />
+              <label for="dpe_energy" class="hasTip" title="' . $energyDesc . '">' . $energyLabel . ' : </label>
+              <input type="text" name="dpe_energy" id="dpe_energy" value="' . $row->dpe_energy . '" class="numberbox" size="5" />
             </li>
             <li>
-              <label for="dpe_ges" class="hasTip" title="' . $gesDesc.'">' . $gesLabel .' : </label>
-              <input type="text" name="dpe_ges" id="dpe_ges" value="'.$row->dpe_ges.'" class="numberbox" size="5" />
+              <label for="dpe_ges" class="hasTip" title="' . $gesDesc . '">' . $gesLabel . ' : </label>
+              <input type="text" name="dpe_ges" id="dpe_ges" value="' . $row->dpe_ges . '" class="numberbox" size="5" />
             </li>
            </ul>
          </fieldset>';
+	}
 
-        echo JHtml::_('sliders.panel', JText::_('PLG_JEA_DPE'), 'dpe-pane');
-        echo $html;
-    }
+	/**
+	 * onAfterShowDescription method
+	 * Called in the default_item.php layout
+	 *
+	 * @param  TableProperty  $row  The property Db row instance
+	 *
+	 * @return void
+	 */
+	public function onAfterShowDescription(&$row)
+	{
+		if ($row->dpe_energy < 0 && $row->dpe_ges < 0)
+		{
+			return;
+		}
 
+		echo '<h3 class="jea_dpe">' . JText::_('PLG_JEA_DPE') . '</h3>' . PHP_EOL;
+		echo '<div class="jea_dpe">' . PHP_EOL;
 
-    /**
-     * onAfterShowDescription method (called in the default_item.php tpl)
-     *
-     * @param stdClass $row
-     */
-    public function onAfterShowDescription(&$row)
-    {
-        if ($row->dpe_energy < 0 && $row->dpe_ges < 0) {
-            return;
-        }
+		if ($row->dpe_energy >= 0)
+		{
+			try
+			{
+				$img = $this->generateEnergyImage($row->dpe_energy);
+				echo '<img src="' . $img . '" alt="' . JText::_('PLG_JEA_DPE_ENERGY_CONSUMPTION') . '" style="margin-right: 50px;" />';
+			}
+			catch (Exception $e)
+			{
+				echo '<strong style="color:red">' . $e->getMessage() . '</strong>';
+			}
+		}
 
-        echo '<h3 class="jea_dpe">'. JText::_('PLG_JEA_DPE') .'</h3>'. PHP_EOL;
+		if ($row->dpe_ges >= 0)
+		{
+			try
+			{
+				$img = $this->generateGESImage($row->dpe_ges);
+				echo '<img src="' . $img . '" alt="' . JText::_('PLG_JEA_DPE_EMISSIONS_GES') . '" />';
+			}
+			catch (Exception $e)
+			{
+				echo '<strong style="color:red">' . $e->getMessage() . '</strong>';
+			}
+		}
 
-        echo '<div class="jea_dpe">'. PHP_EOL;
-        echo getenv('GDFONTPATH');
+		echo '</div>' . PHP_EOL;
+	}
 
+	private function generateEnergyImage($energy = 0)
+	{
+		$lang = JFactory::getLanguage();
+		$tag = $lang->getTag();
+		$imagePath = JPATH_ROOT . '/images/com_jea/dpe/energy-' . $tag . '-' . $energy . '.png';
+		$uri = JFactory::getUri();
+		$imageURL = $uri->root(true) . '/images/com_jea/dpe/energy-' . $tag . '-' . $energy . '.png';
 
-        if ($row->dpe_energy >= 0) {
-            try {
-                $img = $this->__generateEnergyImage($row->dpe_energy);
-                echo '<img src="' . $img . '" alt="'.JText::_('PLG_JEA_DPE_ENERGY_CONSUMPTION').'" style="margin-right: 50px;" />';
-            } catch (Exception $e) {
-                echo '<strong style="color:red">'. $e->getMessage() .'</strong>';
-            }
-        }
+		if (!file_exists($imagePath))
+		{
+			$levels = array(50, 90, 150, 230, 330, 450);
+			$labels = array(
+				'measure' => JText::_('PLG_JEA_DPE_ENERGY_MEASURE'),
+				'top-left' => JText::_('PLG_JEA_DPE_ENERGY_TOP_LEFT_LABEL'),
+				'bottom-left' => JText::_('PLG_JEA_DPE_ENERGY_BOTTOM_LEFT_LABEL')
+			);
 
-        if ($row->dpe_ges >= 0) {
-            try {
-                $img = $this->__generateGESImage($row->dpe_ges);
-                echo '<img src="' . $img . '" alt="'.JText::_('PLG_JEA_DPE_EMISSIONS_GES').'" />';
-            } catch (Exception $e) {
-                echo '<strong style="color:red">'. $e->getMessage() .'</strong>';
-            }
-        }
+			$this->generateGDImage($energy, $imagePath, 'dpe-energy.png', $levels, $labels);
+		}
 
-        echo '</div>'. PHP_EOL;
-    }
+		return $imageURL;
+	}
 
-    private function __generateEnergyImage($energy=0)
-    {
-        $lang = JFactory::getLanguage();
-        $tag = $lang->getTag();
+	private function generateGESImage ($ges = 0)
+	{
+		$lang = JFactory::getLanguage();
+		$tag = $lang->getTag();
+		$imagePath = JPATH_ROOT . '/images/com_jea/dpe/ges-' . $tag . '-' . $ges . '.png';
+		$uri = JFactory::getUri();
+		$imageURL = $uri->root(true) . '/images/com_jea/dpe/ges-' . $tag . '-' . $ges . '.png';
 
-        $imagePath = JPATH_ROOT . '/images/com_jea/dpe/energy-'.$tag .'-'. $energy.'.png';
-        $uri = JFactory::getUri();
-        $imageURL = $uri->root(true) . '/images/com_jea/dpe/energy-'.$tag.'-'.$energy.'.png';
+		if (!file_exists($imagePath))
+		{
+			$levels = array(5, 10, 20, 35, 55, 80);
+			$labels = array(
+				'measure' => JText::_('PLG_JEA_DPE_GES_MEASURE'),
+				'top-left' => JText::_('PLG_JEA_DPE_GES_TOP_LEFT_LABEL'),
+				'bottom-left' => JText::_('PLG_JEA_DPE_GES_BOTTOM_LEFT_LABEL')
+			);
 
-        if (!file_exists($imagePath)) {
-            $levels = array(50,90,150,230,330,450);
-            $labels = array(
-                'measure'     => JText::_('PLG_JEA_DPE_ENERGY_MEASURE'),
-                'top-left'    => JText::_('PLG_JEA_DPE_ENERGY_TOP_LEFT_LABEL'),
-                'bottom-left' => JText::_('PLG_JEA_DPE_ENERGY_BOTTOM_LEFT_LABEL')
-            );
-            $this->__generateGDImage($energy, $imagePath, 'dpe-energy.png', $levels, $labels);
-        }
+			$this->generateGDImage($ges, $imagePath, 'dpe-ges.png', $levels, $labels);
+		}
 
-        return $imageURL;
-    }
+		return $imageURL;
+	}
 
-    private function __generateGESImage($ges=0)
-    {
-        $lang = JFactory::getLanguage();
-        $tag = $lang->getTag();
+	private function generateGDImage ($dpeValue, $imagePath, $imageModel, $levels, $labels = array())
+	{
+		$currentLevel = 0;
+		$imgWidth = 300;
+		$imgHeiht = 260;
+		$fontFile = JPATH_ROOT . '/plugins/jea/dpe/fonts/DejaVuSans.ttf';
+		$fontBoldFile = JPATH_ROOT . '/plugins/jea/dpe/fonts/DejaVuSans-Bold.ttf';
 
-        $imagePath = JPATH_ROOT . '/images/com_jea/dpe/ges-'.$tag .'-'.$ges.'.png';
-        $uri = JFactory::getUri();
-        $imageURL = $uri->root(true) . '/images/com_jea/dpe/ges-'.$tag.'-'.$ges.'.png';
+		foreach ($levels as $level => $value)
+		{
+			if ($dpeValue <= $value)
+			{
+				$currentLevel = $level;
+				break;
+			}
+		}
 
-        if (!file_exists($imagePath)) {
-            $levels = array(5,10,20,35,55,80);
-            $labels = array(
-                'measure'     => JText::_('PLG_JEA_DPE_GES_MEASURE'),
-                'top-left'    => JText::_('PLG_JEA_DPE_GES_TOP_LEFT_LABEL'),
-                'bottom-left' => JText::_('PLG_JEA_DPE_GES_BOTTOM_LEFT_LABEL')
-            );
-            $this->__generateGDImage($ges, $imagePath, 'dpe-ges.png', $levels, $labels);
-        }
+		if ($currentLevel == 0 && $dpeValue > $levels[count($levels) - 1])
+		{
+			$currentLevel = 6;
+		}
 
-        return $imageURL;
-    }
+		$img = @imagecreatetruecolor($imgWidth, $imgHeiht);
 
-    private function __generateGDImage($dpeValue, $imagePath, $imageModel, $levels, $labels=array())
-    {
-        $currentLevel = 0;
-        $imgWidth = 300;
-        $imgHeiht = 260;
-        $fontFile = JPATH_ROOT.'/plugins/jea/dpe/fonts/DejaVuSans.ttf';
-        $fontBoldFile = JPATH_ROOT.'/plugins/jea/dpe/fonts/DejaVuSans-Bold.ttf';
+		if (! $img)
+		{
+			throw new Exception('Cannot create a GD image stream');
+		}
 
-        foreach ($levels as $level => $value) {
-            if ($dpeValue <= $value) {
-                $currentLevel = $level;
-                break;
-            }
-        }
+		$white = imagecolorallocate($img, 255, 255, 255);
+		$grey = imagecolorallocate($img, 200, 200, 200);
+		$grey2 = imagecolorallocate($img, 40, 40, 40);
+		imagefill($img, 0, 0, $white);
+		$arrowImg = @imagecreatefrompng(JPATH_ROOT . '/plugins/jea/dpe/images/arrow.png');
+		$imgModel = @imagecreatefrompng(JPATH_ROOT . '/plugins/jea/dpe/images/' . $imageModel);
 
-        if ($currentLevel == 0 && $dpeValue > $levels[count($levels)-1]) {
-            $currentLevel = 6;
-        }
+		// Where the img model start from Y
+		$destY = ceil(($imgHeiht - imagesy($imgModel)) / 2);
 
-        $img = @imagecreatetruecolor($imgWidth, $imgHeiht);
+		$dpeY = $destY;
 
-        if (!$img) {
-            throw new Exception('Cannot create a GD image stream');
-        }
+		if ($currentLevel == 6)
+		{
+			$dpeY += imagesy($imgModel) - 15;
+		}
+		else
+		{
+			/*
+			 * 30 px height per level + 3px margin
+			 * Adjust now y between the levels limits
+			 */
+			$dpeY += $currentLevel * 33;
+			$start = 0;
+			$end = $levels[$currentLevel];
 
-        $white = imagecolorallocate($img, 255, 255, 255);
-        $grey = imagecolorallocate($img, 200, 200, 200);
-        $grey2 = imagecolorallocate($img, 40, 40, 40);
-        imagefill($img, 0, 0, $white);
+			if (isset($levels[$currentLevel - 1]))
+			{
+				$start = $levels[$currentLevel - 1] + 1;
+			}
 
-        $arrowImg = @imagecreatefrompng(JPATH_ROOT.'/plugins/jea/dpe/images/arrow.png');
-        $imgModel   = @imagecreatefrompng(JPATH_ROOT.'/plugins/jea/dpe/images/'.$imageModel);
+			$dpeY += floor(($dpeValue - $start) * 30 / ($end - $start));
+		}
 
-        // Where the img model start from Y
-        $destY = ceil(($imgHeiht - imagesy($imgModel)) / 2);
+		// Draw horizontal line
+		imageline($img, 0, $dpeY, $imgWidth, $dpeY, $grey);
 
-        $dpeY = $destY;
+		// Draw vertical line
+		imageline($img, 220, 0, 220, $imgHeiht, $grey2);
 
-        if ($currentLevel == 6) {
-            $dpeY += imagesy($imgModel) - 15;
-        } else {
-            $dpeY += $currentLevel * 33; // 30 px height per level + 3px margin
-            // Adjust now y between the levels limits
-            $start = 0;
-            $end = $levels[$currentLevel];
-            if (isset($levels[$currentLevel-1])) {
-                $start = $levels[$currentLevel-1] + 1;
-            }
+		// Copy the image model
+		imagecopy($img, $imgModel, 0, $destY, 0, 0, imagesx($imgModel), imagesy($imgModel));
+		$destX = $imgWidth - imagesx($arrowImg);
+		$destY = $dpeY - (imagesy($arrowImg) / 2);
+		imagecopy($img, $arrowImg, $destX, $destY, 0, 0, imagesx($arrowImg), imagesy($arrowImg));
 
-            $dpeY += floor(($dpeValue - $start) * 30 / ($end - $start));
-        }
+		// Add the value
+		imagettftext($img, 11, 0, $destX + 18, $destY + 20, $white, $fontBoldFile, $dpeValue);
 
-        // Draw horizontal line
-        imageline($img, 0, $dpeY, $imgWidth, $dpeY, $grey);
+		// Add the measure
+		if (isset($labels['measure']))
+		{
+			$box = imagettfbbox(7, 0, $fontFile, $labels['measure']);
+			$x = $box[4] - $box[0];
+			imagettftext($img, 7, 0, $imgWidth - $x, $destY + 41, $grey2, $fontFile, $labels['measure']);
+		}
 
-        // Draw vertical line
-        imageline($img, 220, 0, 220, $imgHeiht, $grey2);
+		// Add text to top left
+		if (isset($labels['top-left']))
+		{
+			imagettftext($img, 8, 0, 0, 9, $grey2, $fontFile, $labels['top-left']);
+		}
 
-        // Copy the image model
-        imagecopy($img, $imgModel, 0, $destY, 0, 0, imagesx($imgModel), imagesy($imgModel));
-        $destX = $imgWidth - imagesx($arrowImg);
-        $destY = $dpeY - (imagesy($arrowImg) / 2);
-        imagecopy($img, $arrowImg, $destX, $destY, 0, 0, imagesx($arrowImg), imagesy($arrowImg));
+		// Add text to top right
+		imagettftext($img, 8, 0, 230, 9, $grey2, $fontFile, JText::_('PLG_JEA_DPE_TOP_RIGHT_LABEL'));
 
-        // Add the value
-        imagettftext($img, 11, 0 ,$destX+18 ,$destY+20 ,$white ,$fontBoldFile ,$dpeValue);
+		// Add text to bottom left
+		if (isset($labels['bottom-left']))
+		{
+			imagettftext($img, 8, 0, 0, $imgHeiht - 3, $grey2, $fontFile, $labels['bottom-left']);
+		}
 
-        // Add the measure
-        if (isset($labels['measure'])) {
-            $box = imagettfbbox(7, 0, $fontFile, $labels['measure']);
-            $x = $box[4] - $box[0];
-            imagettftext($img, 7, 0, $imgWidth-$x ,$destY+41 ,$grey2 ,$fontFile , $labels['measure']);
-        }
+		$ret = @imagepng($img, $imagePath);
 
-        // Add text to top left
-        if (isset($labels['top-left'])) {
-            imagettftext($img, 8, 0 ,0 ,9 ,$grey2 ,$fontFile , $labels['top-left']);
-        }
+		imagedestroy($img);
+		imagedestroy($arrowImg);
+		imagedestroy($imgModel);
 
-        // Add text to top right 
-        imagettftext($img, 8, 0 ,230 ,9 ,$grey2 ,$fontFile , JText::_('PLG_JEA_DPE_TOP_RIGHT_LABEL'));
-
-        // Add text to bottom left
-        if (isset($labels['bottom-left'])) {
-            imagettftext($img, 8, 0 ,0 , $imgHeiht-3 ,$grey2 ,$fontFile , $labels['bottom-left']);
-        }
-
-        $ret = @imagepng($img, $imagePath);
-
-        imagedestroy($img);
-        imagedestroy($arrowImg);
-        imagedestroy($imgModel);
-
-        if (!$ret) {
-            throw new Exception('Cannot save image : '. $imagePath);
-        }
-    }
-
+		if (!$ret)
+		{
+			throw new Exception('Cannot save image : ' . $imagePath);
+		}
+	}
 }
--- script.php
+++ script.php
@@ -1,98 +1,94 @@
 <?php
 /**
- * This file is part of Joomla Estate Agency - Joomla! extension for real estate agency
+ * @package     Joomla.Plugin
+ * @subpackage  Jea.Dpe
  *
- * @version     $Id: script.php 257 2012-02-05 23:04:04Z ilhooq $
- * @package		Jea
- * @copyright	Copyright (C) 2008 PHILIP Sylvain. All rights reserved.
- * @license		GNU/GPL, see LICENSE.txt
- * Joomla Estate Agency is free software. This version may have been modified pursuant to the
- * GNU General Public License, and as distributed it includes or is derivative
- * of works licensed under the GNU General Public License or other free or open
- * source software licenses.
- *
+ * @copyright   Copyright (C) 2007 - 2019 PHILIP Sylvain. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
 
-jimport('joomla.filesystem.folder');
 /**
  * Install Script file of JEA component
  */
 class plgjeadpeInstallerScript
 {
-    /**
-     * method to install the extension
-     *
-     * @return void
-     */
-    function install($parent)
-    {
-        if (!JFolder::exists(JPATH_ROOT . '/images/com_jea/dpe')) {
-            JFolder::create(JPATH_ROOT . '/images/com_jea/dpe');
-        }
-    }
+	/**
+	 * Method to install the extension
+	 *
+	 * @return void
+	 */
+	function install ($parent)
+	{
+		if (!JFolder::exists(JPATH_ROOT . '/images/com_jea/dpe'))
+		{
+			JFolder::create(JPATH_ROOT . '/images/com_jea/dpe');
+		}
+	}
 
-    /**
-     * method to uninstall the extension
-     *
-     * @return void
-     */
-    function uninstall($parent)
-    {
-        if (JFolder::exists(JPATH_ROOT . '/images/com_jea/dpe')) {
-            JFolder::delete(JPATH_ROOT . '/images/com_jea/dpe');
-        }
-    }
+	/**
+	 * Method to uninstall the extension
+	 *
+	 * @return void
+	 */
+	function uninstall ($parent)
+	{
+		if (JFolder::exists(JPATH_ROOT . '/images/com_jea/dpe'))
+		{
+			JFolder::delete(JPATH_ROOT . '/images/com_jea/dpe');
+		}
+	}
 
-    /**
-     * method to update the extension
-     *
-     * @return void
-     */
-    function update($parent)
-    {
-        if (!JFolder::exists(JPATH_ROOT . '/images/com_jea/dpe')) {
-            JFolder::create(JPATH_ROOT . '/images/com_jea/dpe');
-        }
-    }
+	/**
+	 * Method to update the extension
+	 *
+	 * @return void
+	 */
+	function update ($parent)
+	{
+		if (! JFolder::exists(JPATH_ROOT . '/images/com_jea/dpe'))
+		{
+			JFolder::create(JPATH_ROOT . '/images/com_jea/dpe');
+		}
+	}
 
-    /**
-     * method to run before an install/update/uninstall method
-     *
-     * @return void
-     */
-    function preflight($type, $parent)
-    {
-        $db = JFactory::getDbo();
-        $db->setQuery('SHOW COLUMNS FROM #__jea_properties');
-        $cols = $db->loadObjectList('Field');
-        if(!isset($cols['dpe_energy']) && !isset($cols['dpe_ges'])){
-            $query = 'ALTER TABLE `#__jea_properties` '
-            . "ADD `dpe_energy` SMALLINT(4) NOT NULL DEFAULT '-1',"
-            . "ADD `dpe_ges` SMALLINT(4) NOT NULL DEFAULT '-1'";
+	/**
+	 * Method to run before an install/update/uninstall method
+	 *
+	 * @return void
+	 */
+	function preflight ($type, $parent)
+	{
+		$db = JFactory::getDbo();
+		$db->setQuery('SHOW COLUMNS FROM #__jea_properties');
+		$cols = $db->loadObjectList('Field');
 
-            $db->setQuery($query);
-            $db->query();
-        } elseif (isset($cols['dpe_energie']) && isset($cols['dpe_ges'])) {
-            $query = 'ALTER TABLE `#__jea_properties` '
-                   . "CHANGE `dpe_energie` `dpe_energy` SMALLINT(4) NOT NULL DEFAULT '-1',"
-                   . "CHANGE `dpe_ges` `dpe_ges` SMALLINT(4) NOT NULL DEFAULT '-1'";
-            $db->setQuery($query);
-            $db->query();
-        }
-    }
+		if (! isset($cols['dpe_energy']) && ! isset($cols['dpe_ges']))
+		{
+			$query = 'ALTER TABLE `#__jea_properties` ' . "ADD `dpe_energy` SMALLINT(4) NOT NULL DEFAULT '-1',"
+					. "ADD `dpe_ges` SMALLINT(4) NOT NULL DEFAULT '-1'";
+			$db->setQuery($query);
+			$db->query();
+		}
+		elseif (isset($cols['dpe_energie']) && isset($cols['dpe_ges']))
+		{
+			$query = 'ALTER TABLE `#__jea_properties` ' . "CHANGE `dpe_energie` `dpe_energy` SMALLINT(4) NOT NULL DEFAULT '-1',"
+					. "CHANGE `dpe_ges` `dpe_ges` SMALLINT(4) NOT NULL DEFAULT '-1'";
+			$db->setQuery($query);
+			$db->query();
+		}
+	}
 
-    /**
-     * method to run after an install/update/uninstall method
-     *
-     * @return void
-     */
-    function postflight($type, $parent)
-    {
-
-    }
+	/**
+	 * Method to run after an install/update/uninstall method
+	 *
+	 * @return void
+	 */
+	function postflight ($type, $parent)
+	{
+	}
 }
 
 
